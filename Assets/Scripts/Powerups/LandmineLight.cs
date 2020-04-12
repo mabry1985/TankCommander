5081821862bb2b926598183161a3beedec89d158
Josh Mabry <mabry1985@gmail.com>
2020-04-12 14:51:52 -0700

    add landmine powerup

---
+++ Assets/Scripts/Powerups/LandmineLight.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandmineLight : MonoBehaviour
+{
+    public float blinkTime = 2f;
+
+    Color lerpedColor = Color.white;
+
+    private Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        lerpedColor = Color.Lerp(Color.red, Color.black, Mathf.PingPong(Time.time, 1));
+        renderer.material.color = lerpedColor;
+
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            renderer.material.color = Color.black;
+            yield return new WaitForSeconds(blinkTime);
+            renderer.material.color = Color.red;
+            yield return new WaitForSeconds(blinkTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Powerups/PowerUpRotate.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PowerUpRotate : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(0, .5f, 0);
+    }
+}
