59ea07d5def1ff2dbd45d7fc608cb49a799c3141
gnostichumor <preston.matt86@gmail.com>
2020-04-22 12:38:11 -0700

    added new scene to test GameManager single script to run coin gathering and store functionality

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Manage Coin Coint and Updating Coin Text
+//Manage Store Open/Close Feature
+
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
