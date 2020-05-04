83b87489b5221ec07db4f753c4ed195c5702ee09
Josh Mabry <mabry1985@gmail.com>
2020-05-04 13:37:49 -0700

    refactor file structure

---
+++ Assets/Scripts/MainMenu/ButtonManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        LoadingStateManager.Load(LoadingStateManager.Scene.Tanks);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
