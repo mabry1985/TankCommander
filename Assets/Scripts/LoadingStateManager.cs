3788fa05ec70e464f10d87da642afc922c5a66d8
Josh Mabry <mabry1985@gmail.com>
2020-05-04 13:02:23 -0700

    build out loading state machine

---
+++ Assets/Scripts/LoadingStateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LoadingStateManager
+{
+    public enum Scene
+    {
+        MainMenu,
+        Tanks
+    }
+
+    public static void Load(Scene scene)
+    {
+        SceneManager.LoadScene(scene.ToString());
+    }
+}
