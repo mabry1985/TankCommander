e378786c0797e13fa95eea96acf796a43babff6a
Josh Mabry <mabry1985@gmail.com>
2020-05-04 12:12:49 -0700

    add animation script for button press and wire up quit functionality

---
+++ Assets/Scripts/UI/ButtonInteraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour
+{
+
+    public Button button;
+    public Text text;
+
+    public void ButtonAnimate()
+    {
+        text.fontSize = 25;
+        StartCoroutine(ResetFontSize(0.25f));
+    }
+
+    IEnumerator ResetFontSize(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        text.fontSize = 30;
+    }
+    
+}
