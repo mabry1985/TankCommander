e280e3367be90a8b43ab9e6da667ffd7ffd2978e
Josh Mabry <mabry1985@gmail.com>
2020-05-04 17:49:07 -0700

    add loading script for text

--- Assets/Scripts/LoadingText.cs
+++ Assets/Scripts/LoadingText.cs
@@ -9,18 +9,33 @@ public class LoadingText : MonoBehaviour
     public MeshRenderer sphere2;
     public MeshRenderer sphere3;
 
+    public float seconds = 0.4f;
+
     // Start is called before the first frame update
     void Start()
+    {
+        DisableElipsis();
+        StartCoroutine(AnimateElipsis(seconds));
+    }
+
+    private IEnumerator AnimateElipsis(float sec)
+    {
+        while (true) {
+            sphere1.enabled = true;
+            yield return new WaitForSeconds(sec);
+            sphere2.enabled = true;
+            yield return new WaitForSeconds(sec);
+            sphere3.enabled = true;
+            yield return new WaitForSeconds(sec);
+            DisableElipsis();
+            yield return new WaitForSeconds(sec);
+        }
+    }
+
+    private void DisableElipsis ()
     {
         sphere1.enabled = false;
         sphere2.enabled = false;
         sphere3.enabled = false;
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
