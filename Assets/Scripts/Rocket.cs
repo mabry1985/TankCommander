21f764ad974bfba0e1964fc7635aad02e7abe27f
Josh Mabry <mabry1985@gmail.com>
2020-05-14 12:34:26 -0700

    add rocket prefab and script for firing

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public GameObject rocket;
+    public float speed = 3f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rocket.transform.Translate(0, 5 * Time.deltaTime * speed, 0);
+        rocket.transform.Rotate(0f, .05f, 0f, Space.Self);
+    }
+}
---
+++ Assets/Scripts/RocketMount.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMount : MonoBehaviour
+{
+
+    public GameObject mountedRocket1;
+    public GameObject mountedRocket2;
+
+    public GameObject rocketPrefab;
+
+    private bool rocket1Fired = false;
+    private bool empty = false;
+
+    private void Awake() 
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void FireRocket() 
+    {
+        if (!rocket1Fired) {
+            mountedRocket1.SetActive(false);
+            GameObject rocket = Instantiate(rocketPrefab, transform.position, transform.rotation);
+            rocket1Fired = true;
+        } else if (!empty) {
+            mountedRocket2.SetActive(false);
+            GameObject rocket = Instantiate(rocketPrefab, transform.position, transform.rotation);
+            empty = true;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
